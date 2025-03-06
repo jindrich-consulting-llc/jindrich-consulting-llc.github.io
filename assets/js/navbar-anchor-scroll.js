95c89f8ae7b2cacffa093a6397963e4d1b81a3b7
PPjamies <pazuzujindrich@gmail.com>
2025-03-05 18:20:25 -0800

    fixed navbar rendering issue when navigation linked is clicked.

--- assets/js/navbar-anchor-scroll.js
+++ assets/js/navbar-anchor-scroll.js
@@ -1,9 +1,21 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+document.addEventListener('DOMContentLoaded', function() {
+    const anchorLinks = document.querySelectorAll('a[href^="#"]'); // Select all anchor links
+
+    console.log(anchorLinks)
+
+    anchorLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default jump
+
+            const targetId = this.getAttribute('href').substring(1); // Get the target ID
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
         });
     });
 });
